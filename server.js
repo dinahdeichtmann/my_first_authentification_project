477e796337350d96c7d0c3913b4f41e52f6452f3
Dinah Deichtmann <dinahdeichtmann@gmail.com>
2023-01-13 15:46:54 +0100

    register functionality done (no database)

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+
 const app = express();
 
+const initializePassport = require("./passport_config");
+initializePassport(passport);
+
+const users = [];
+
 app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { name: "Dinah" });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post("/login", (req, res) => {});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+  }
+  console.log(users);
 });
 
 app.listen(3000);
